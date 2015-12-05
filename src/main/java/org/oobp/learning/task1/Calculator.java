2b9f7ed7bc3a9c024205b8c91069fd4a6b5ef69f
Dagmara <179792@pwr.wroc.student.pl>
2015-12-05 15:01:40 +0100

    Constructor added.

--- src/main/java/org/oobp/learning/task1/Calculator.java
+++ src/main/java/org/oobp/learning/task1/Calculator.java
@@ -9,8 +9,16 @@ public class Calculator {
     private int digitB;
 
     //Constructor methods take the same name as the class
-    Calculator(){}
+    Calculator(){
+        digitA = 5;
+        digitB = 6;
+    }
 
+    Calculator(int digitA, int digitB){
+        this.digitA = digitA;
+        this.digitB = digitB;
+    }
+    
     public int sum() {
         return digitA + digitB;
     }
