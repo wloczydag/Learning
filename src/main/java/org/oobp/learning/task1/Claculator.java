0cbbdbc1daab2cfe7a1325d1e1bd7a2b044ad5b4
Dagmara <179792@pwr.wroc.student.pl>
2015-12-05 14:52:01 +0100

    New project created.

---
+++ src/main/java/org/oobp/learning/task1/Claculator.java
@@ -0,0 +1,29 @@
+package org.oobp.learning.task1;
+
+/**
+ * Created by Dagmara on 2015-12-05.
+ */
+public class Claculator {
+
+    int digitA;
+    int digitB;
+
+    public int sum() {
+        return digitA + digitB;
+    }
+    public int getDigitA() {
+        return digitA;
+    }
+
+    public void setDigitA(int digitA) {
+        this.digitA = digitA;
+    }
+
+    public int getDigitB() {
+        return digitB;
+    }
+
+    public void setDigitB(int digitB) {
+        this.digitB = digitB;
+    }
+}
